33f12d9e04b98bebe6fceae55ff7bff7cc227dd8
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-04-03 19:14:36 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using Sistema_UC12.Classes;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        Pessoa novaPessoa = new Pessoa();
+        novaPessoa.endereco = "rua niterou,1801";
+        novaPessoa.nome = "Caique";
+        novaPessoa.enderecoComercial = true;
+
+        Console.WriteLine(novaPessoa.nome);
+    }
+}
+
