427252fd1ab46880a4419c0cf35c5d1af3a11379
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-04-12 16:31:15 -0300

    concatenação

--- Classes/PessoaFisica.cs
+++ Classes/PessoaFisica.cs
@@ -16,12 +16,24 @@ public class PessoaFisica : Pessoa //Utilizar : para que as instâncias tenham a
 
     public DateTime DataNascimento { get; set; }
 
-    public override void PagarImposto (){ //Colocamos o override para sobre escrever o método PagarImposto, pois ele já existe na classe-mãe
+    public override void PagarImposto (float rendimento){ //Colocamos o override para sobre escrever o método PagarImposto, pois ele já existe na classe-mãe
+
+    }
+
+    public bool ValidarDataNascimento (DateTime dataNasc){
+        DateTime dataAtual = DateTime.Today;
+
+        double anos = (dataAtual-dataNasc).TotalDays / 365; //Fórmula para obter a idade de uma pessoa
+
+        if (anos >= 18){
+            return true;
+        } else {
+            return false;
+        }
 
     }
     
  }
 }
 
-}
 
--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@ class Program
     static void Main(string[] args) //Atalho svm + Enter
 {
 
-PessoaFisica Caique = new PessoaFisica ();
-Caique.Nome = "Caique";
+Endereco end = new Endereco ();
 
-Console.WriteLine(Caique.Nome);
+end.logradouro = "Rua Niterói";
+end.numero = 180;
+end.complemento = "Escola SENAI";
+end.enderecoComercial = true;
+
+PessoaFisica novapf = new PessoaFisica ();
+novapf.cpf = "080.733.239-90";
+novapf.endereco = end;
+novapf.DataNascimento = new DateTime (1991,06,16); //Padrão americano= Ano; mês; dia
+novapf.Nome = "Ygor Andrade";
+
+//Console.WriteLine (novapf.endereco.logradouro);
+//Console.WriteLine (novapf.endereco.numero);
+//Console.WriteLine (novapf.endereco.complemento);
+//Console.WriteLine (novapf.endereco.enderecoComercial);
+
+//Console.WriteLine (novapf.Nome);
+//Console.WriteLine (novapf.DataNascimento);
+
+//Interpolação
+
+Console.WriteLine($"O endereço do {novapf.Nome} é {novapf.endereco.logradouro}, nº {novapf.endereco.numero}.");
+
+//Concatenação
+
+Console.WriteLine ("O endereço do "+ novapf.Nome + " é " + novapf.endereco.logradouro + ", nº " + novapf.endereco.numero +".");
 
 }
 
